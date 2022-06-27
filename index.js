909df8f4806dcef2ef3b07ec5227ea1dcd6722eb
muhid <muhid@ihealthscreen.org>
2022-06-27 23:44:44 +0600

    os add

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const _ = require('lodash');
+const os = require('os');
 const man = require('./man');
 
 const a = 10;
 console.log(_.last(man.array1));
 console.log(man.family(a));
+console.log(os.platform());
+console.log(os.uptime());
