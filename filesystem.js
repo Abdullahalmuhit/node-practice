d78da9b1de613968b56da7164b2df9e5c2d460f4
muhid <muhid@ihealthscreen.org>
2022-06-28 21:11:20 +0600

    value

--- filesystem.js
+++ filesystem.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 const man = require('./man');
+// // write file
+// fs.writeFileSync('filesystem1.txt', man.array1);
+// // append file
+// fs.appendFile('filesystem1.txt', man.name + man.age, (err) => {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+// read file
+fs.readFile('filesystem1.txt', (err, data) => {
+    console.log(data.toString());
+});
 
-fs.writeFileSync('filesystem1.txt', man.array1);
-fs.appendFile('filesystem1.txt', man.name + man.age, (err) => {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+console.log('muhit');
