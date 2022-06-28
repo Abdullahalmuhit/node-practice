db01b4729ea28389f4c2a05c2582ca1259e688d9
muhid <muhid@ihealthscreen.org>
2022-06-28 23:09:27 +0600

    server create

--- http.js
+++ http.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello how are you?');
-    res.end();
+    if (req.url === '/about-us') {
+        res.write('This is about us page');
+        res.end();
+    } else if (req.url === '/') {
+        res.write('Hello how are you?');
+        res.end();
+    } else {
+        res.write('page not found');
+        res.end();
+    }
 });
 
 server.listen(30001);
-console.log('server running');
+console.log('server running.....');
