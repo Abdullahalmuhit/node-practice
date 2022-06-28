b259d32a4b750ca3a89511e3307b29402bf1f241
Muhid <muhid@ihealthscreen.org>
2022-06-28 10:35:00 +0600

    Added file system

--- filesystem.js
+++ filesystem.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 const man = require('./man');
 
 fs.writeFileSync('filesystem1.txt', man.array1);
+fs.appendFile('filesystem1.txt', man.name + man.age, (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+  });
